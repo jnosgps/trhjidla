dd666939762598d4365aff77715ff2ac126392cd
jnosgps <baxgtx@gmail.com>
2017-02-23 15:36:11 +0100

    admin/register Pageinfo

--- shop1/admin.py
+++ shop1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Producer, Product, Customer, Member, Order, OrderItem
+from .models import Category, Producer, Product, Customer, Member, Order, OrderItem, PageInfo
 
 admin.site.register(Category)
 admin.site.register(Producer)
@@ -8,3 +8,4 @@ admin.site.register(Customer)
 admin.site.register(Member)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(PageInfo)
