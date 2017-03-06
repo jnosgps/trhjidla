a8917b91f8ea62d594e5e5bbd7a6622ebcd22ec1
jnosgps <baxgtx@gmail.com>
2017-03-06 17:40:19 +0100

    tjpm: loginpage-jscript

--- static/js/tjapi.tjpm.js
+++ static/js/tjapi.tjpm.js
@@ -8,25 +8,31 @@ $("#tjpm-signin").on('click', function(e) {
 	e.preventDefault();
 	console.log('SignIn called...');
 	if ($('#tjpm-login').val() == '') {
-		console.log('tjpm-login is blank...');}
+		console.log('tjpm-login is blank...');
+	}
 	else if ($('#tjpm-pass').val() == '') {
-		console.log('tjpm-pass is blank...');}
+		console.log('tjpm-pass is blank...');
+	}
 	else {
 		console.log('doing login...');
 		hasher.update($('#tjpm-pass').val());
 		var postdata = {'login': $('#tjpm-login').val(), 'password': hasher.getHash("HEX"), 'csrfmiddlewaretoken': csrf_token};
 		console.log('post data: ' + postdata);
 		$.post("login/", postdata, function(data, status) {
-			if (status == 'success') { location.reload(true); }
-			else alert("Data: " + data + "\nStatus: " + status);
+			if (status == 'success') { 
+				location.reload(true);
+			}
+			else {
+				alert("Data: " + data + "\nStatus: " + status);
+			}
 		});
 		console.log('everything has been done');
 	}
 });
 
-$("#tjpm-signout").on('click', function(e) {
+/*$("#tjpm-signout").on('click', function(e) {
 	$.post('logout/', {'csrfmiddlewaretoken': csrf_token}, function(data, status) {
 		if (status == 'success') { location.reload(true); }
 		else alert("Data: " + data + "\nStatus: " + status);
 	});
-});
+});*/
