af5828960d12e1ab58bf453f93e3f3e83c177403
jnosgps <baxgtx@gmail.com>
2017-03-09 20:35:30 +0100

    ...

--- dat/models.py
+++ dat/models.py
@@ -8,17 +8,17 @@ class Wallet(models.Model):
 	driver = models.ForeignKey('dat.Driver')
 	got = models.IntegerField(default=0)
 	given = models.IntegerField(default=0)
-	started_date = models.DateTimeField(default=timezone.now())
-	last_update = models.DateTimeField(default=timezone.now())
+	started_date = models.DateTimeField(default=timezone.now)
+	last_update = models.DateTimeField(default=timezone.now)
 	
 	def deposit(self, amount):
 		self.got += amount
-		self.last_update = timezone.now()
+		self.last_update = timezone.now
 		self.save()
 	
 	def withdraw(self, amount):
 		self.given += amount
-		self.last_update = timezone.now()
+		self.last_update = timezone.now
 		self.save()
 	
 	def __unicode__(self):
@@ -30,7 +30,7 @@ class Driver(models.Model):
 	name = models.CharField(max_length=64)
 	lat = models.DecimalField(max_digits=10, decimal_places=6, default=0)
 	lng = models.DecimalField(max_digits=10, decimal_places=6, default=0)
-	registered_date = models.DateTimeField(default=timezone.now())
+	registered_date = models.DateTimeField(default=timezone.now)
 	last_active = models.DateTimeField(blank=True, null=True)
 	status = models.IntegerField(default=0)
 	level = models.IntegerField(default=0)
@@ -38,12 +38,12 @@ class Driver(models.Model):
 	def updateLocation(self, lat, lng):
 		self.lat = lat
 		self.lng = lng
-		self.last_active = timezone.now()
+		self.last_active = timezone.now
 		self.save()
 	
 	def changeStatus(self, status):
 		self.status = status
-		self.last_active = timezone.now()
+		self.last_active = timezone.now
 		self.save()
 	
 	def __unicode__(self):
