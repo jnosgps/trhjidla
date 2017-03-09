f3000a9427cd3d845a866e735ad5e38ddf34c933
Lukas Novotny <lukas.novotny@trhjidla.cz>
2017-03-09 20:15:48 +0100

    novej projektgit add . -- dat- driver asset tracking

--- static/js/tjapi.1.js
+++ static/js/tjapi.1.js
@@ -7,3 +7,44 @@ $('.dropdown').on('show.bs.dropdown', function() {
 $('.dropdown').on('hide.bs.dropdown', function() {
 	$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
 });
+
+$('#algo-hash').hide();
+$('#algo').on('click', function() {
+	var q = prompt('Co chceš zahashovat?');
+	if (q) {
+		$.get('algo/?q=' + q, function(data, status) {
+			if (status == 'success') {
+				alert(data)
+			}
+		});
+	}
+});
+
+
+// POST handlers
+$("#login-a").click(function() {
+	if ($('#login-login').val() == '') {}
+	else if ($('#login-password').val() == '') {}
+	else {
+		var hasher = new jsSHA("SHA-256", "TEXT");
+		hasher.update($('#login-password').val());
+		var postdata = {'login': $('#login-login').val(), 'password': hasher.getHash("HEX"), 'csrfmiddlewaretoken': csrf_token};
+		$.post("login/", postdata, function(data, status) {
+			if (status == 'success') location.reload(true);
+			else alert("Data: " + data + "\nStatus: " + status);
+		});
+	}
+});
+$("#logout-a").click(function() {
+	$.post('logout/', {'csrfmiddlewaretoken': csrf_token}, function(data, status) {
+		if (status == 'success') location.reload(true);
+		else alert("Data: " + data + "\nStatus: " + status);
+	});
+});
+
+$('.backbutton').on('click', function() {
+	window.history.back();
+});
+$('.twobackbutton').on('click', function() {
+	window.history.go(-2);
+});
