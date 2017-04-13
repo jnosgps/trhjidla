b907882d27cc3d31b9f859e3219fb617b929c357
jnosgps <baxgtx@gmail.com>
2017-04-13 12:13:52 +0200

    shop1: z views presunuty konstanti pole do const.py, zkouska razeni producentu (vzdy nejdrive on)

---
+++ shop1/const.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+katfam = {
+	'fastfood': u'Rychlé občerstvení',
+	'freshfood': u'Zdravé jídlo',
+	'sweets': u'Zákusky',
+	'raws': u'Suroviny',
+	'direct': u'Přímý prodej',
+	'drinks': u'Nápoje',
+}
+katimg = {
+	'fastfood': '/static/img/dlazdice/fastfood.svg',
+	'freshfood': '/static/img/dlazdice/freshfood.svg',
+	'sweets': '/static/img/dlazdice/sweets.svg',
+	'raws': '/static/img/dlazdice/raws.svg',
+	'direct': '/static/img/dlazdice/direct.svg',
+	'drinks': '/static/img/dlazdice/fastfood.svg',
+}
+katimg2 = {
+	'fastfood': '/static/img/dlazdice_green/fastfood.svg',
+	'freshfood': '/static/img/dlazdice_green/freshfood.svg',
+	'sweets': '/static/img/dlazdice_green/sweets.svg',
+	'raws': '/static/img/dlazdice_green/raws.svg',
+	'direct': '/static/img/dlazdice_green/direct.svg',
+	'drinks': '/static/img/dlazdice_green/fastfood.svg',
+}
--- shop1/views.py
+++ shop1/views.py
@@ -7,30 +7,7 @@ from .models import Category, Producer, Product, Member, Customer, Order, OrderI
 import hashlib
 from django.utils import timezone
 
-katfam = {
-	'fastfood': u'Rychlé občerstvení',
-	'freshfood': u'Zdravé jídlo',
-	'sweets': u'Zákusky',
-	'raws': u'Suroviny',
-	'direct': u'Přímý prodej',
-	'drinks': u'Nápoje',
-}
-katimg = {
-	'fastfood': '/static/img/dlazdice/fastfood.svg',
-	'freshfood': '/static/img/dlazdice/freshfood.svg',
-	'sweets': '/static/img/dlazdice/sweets.svg',
-	'raws': '/static/img/dlazdice/raws.svg',
-	'direct': '/static/img/dlazdice/direct.svg',
-	'drinks': '/static/img/dlazdice/fastfood.svg',
-}
-katimg2 = {
-	'fastfood': '/static/img/dlazdice_green/fastfood.svg',
-	'freshfood': '/static/img/dlazdice_green/freshfood.svg',
-	'sweets': '/static/img/dlazdice_green/sweets.svg',
-	'raws': '/static/img/dlazdice_green/raws.svg',
-	'direct': '/static/img/dlazdice_green/direct.svg',
-	'drinks': '/static/img/dlazdice_green/fastfood.svg',
-}
+from .const import katfam, katimg, katimg2
 
 def tryToGetMember(request):
 	member = {}
@@ -138,9 +115,9 @@ def producers_list(request, kategorie='fastfood', razeni='az'):
 	infotext = PageInfo.objects.get().info_text
 		
 	if razeni == 'az':
-		restaurace = Producer.objects.filter(product__category__name=kategorie).order_by('name').distinct()
+		restaurace = Producer.objects.filter(product__category__name=kategorie).order_by('online').distinct().order_by('name').di
 	elif razeni == 'top':
-		restaurace = Producer.objects.filter(product__category__name=kategorie).order_by('registered_date').distinct()
+		restaurace = Producer.objects.filter(product_category__name=kategorie).order_by('online').distinct().order_by('registered_date').distinct()
 	else:
 		restaurace  = Producer.objects.filter(product__category__name=kategorie).order_by('online').distinct()
 	
