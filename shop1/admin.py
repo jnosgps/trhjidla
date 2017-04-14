9f17ebc088cbe40a470f9aed77fceb68103a1632
jnosgps <baxgtx@gmail.com>
2017-04-14 14:27:45 +0200

    ordersubstatus model

--- shop1/admin.py
+++ shop1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Producer, Product, Customer, Member, Order, OrderItem, PageInfo, LabelTag, FoodType
+from .models import Category, Producer, Product, Customer, Member, Order, OrderItem, OrderSubstatus, PageInfo, LabelTag, FoodType
 
 admin.site.register(Category)
 admin.site.register(Producer)
@@ -8,6 +8,7 @@ admin.site.register(Customer)
 admin.site.register(Member)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(OrderSubstatus)
 admin.site.register(PageInfo)
 admin.site.register(LabelTag)
 admin.site.register(FoodType)
