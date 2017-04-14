023cd29cb28c33893a68b26371bd6285e45068ad
jnosgps <baxgtx@gmail.com>
2017-04-14 10:53:03 +0200

    razeni on/off

--- shop1/views.py
+++ shop1/views.py
@@ -115,9 +115,11 @@ def producers_list(request, kategorie='fastfood', razeni='az'):
 	infotext = PageInfo.objects.get().info_text
 		
 	if razeni == 'az':
-		restaurace = Producer.objects.filter(product__category__name=kategorie).order_by('name', '-online').distinct()
+		#restaurace = Producer.objects.filter(product__category__name=kategorie).order_by('name', '-online').distinct()
+		restaurace = Producer.objects.filter(product__category__name=kategorie).order_by('-online', 'name').distinct()
 	elif razeni == 'top':
-		restaurace = Producer.objects.filter(product_category__name=kategorie).order_by('-registered_date', '-online').distinct()
+		#restaurace = Producer.objects.filter(product_category__name=kategorie).order_by('-registered_date', '-online').distinct()
+		restaurace = Producer.objects.filter(product_category__name=kategorie).order_by('-online', '-registered_date').distinct()
 	else:
 		restaurace  = Producer.objects.filter(product__category__name=kategorie).order_by('-online').distinct()
 	
